2bdbf90542f524c37bb38f2786da341a64baba58
michaele490 <michaelegharevba490@gmail.com>
2025-04-29 16:43:39 +0100

    Weight class logic on boxer model

--- app/models/boxer.rb
+++ app/models/boxer.rb
@@ -7,6 +7,27 @@ class Boxer < ApplicationRecord
 
   belongs_to :coach, optional: true
 
+  WEIGHT_CLASSES = [
+    "minimum",
+    "light fly",
+    "fly",
+    "super fly",
+    "bantam",
+    "super bantam",
+    "feather",
+    "super feather",
+    "light",
+    "super light",
+    "welter",
+    "super welter",
+    "middle",
+    "super middle",
+    "light heavy",
+    "cruiser",
+    "heavy"
+  ]
+
+  validates :weight_class, inclusion: { in: WEIGHT_CLASSES }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true, uniqueness: true
