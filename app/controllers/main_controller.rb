96314711760dd6a082d1615d093192d090563e55
michaele490 <michaelegharevba490@gmail.com>
2025-05-09 08:15:16 +0100

    Results page working

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,6 +6,24 @@ class MainController < ApplicationController
     end
 
     def results
+        @fights = Fight.includes(:boxer_a, :boxer_b).where(status: 'occurred')
+
+        if params[:division].present? && params[:division] != ""
+            @fights = @fights.where(weight_class: params[:division])
+        end
+
+        if params[:gender].present? && params[:gender] != ""
+            @fights = @fights.select { |f| f.boxer_a.gender == params[:gender] || f.boxer_b.gender == params[:gender] }
+        end
+
+        if params[:search].present? && params[:search] != ""
+            search = params[:search].downcase
+            @fights = @fights.select do |f|
+                f.boxer_a.full_name.downcase.include?(search) || f.boxer_b.full_name.downcase.include?(search)
+            end
+        end
+
+        @fights = @fights.sort_by { |f| f.fight_date || Date.new(1900,1,1) }.reverse
     end
 
     def fixtures
